46e01e0f9480ab4290028c00cbf3b11121aa25fa
prasad <prasad4308@gmail.com>
2016-07-31 11:37:32 +0530

    project

---
+++ src/GenEvenNumber.java
@@ -0,0 +1,11 @@
+public class GenEvenNumber
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println("Number: " + i + "is: " + i);
+			System.out.println("Number: " + i + "is: " + i);
+		}	
+    }
+}
